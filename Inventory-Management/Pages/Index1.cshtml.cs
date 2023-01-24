9f5c106e79f09a6fd37084a3a53f74217b7588ea
Rhythm Sapkota <113872162+RhythmSapkota@users.noreply.github.com>
2023-01-24 07:21:08 +0545

    First

---
+++ Inventory-Management/Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Inventory_Management.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
